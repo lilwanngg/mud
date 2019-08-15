fe679c9e8094f5a12122dda10cc05db88f82f4c8
lilwanngg <lillianwang@utexas.edu>
2019-08-15 17:08:32 -0400

    Change views to html

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname));
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
