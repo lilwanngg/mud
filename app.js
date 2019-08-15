75b315cf7e635598f923bf37e98f241c8ff28c1f
lilwanngg <lillianwang@utexas.edu>
2019-08-15 17:01:38 -0400

    Change views placement

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var index = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
