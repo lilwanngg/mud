c156a2b126f415fe1c73d19d49d8c10c7bcab762
lilwanngg <lillianwang@utexas.edu>
2019-08-20 00:03:49 -0400

    Fix port issue hopefully

--- bin/server.js
+++ bin/server.js
@@ -2,11 +2,24 @@ var app = require('../app');
 var debug = require('debug')
 var http = require('http');
 
-var port = process.env.PORT || '3000';
-app.set('port', process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 var server = http.createServer(app);
+
 server.listen(port);
 server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
 
 function onListening() {
   var addr = server.address();
