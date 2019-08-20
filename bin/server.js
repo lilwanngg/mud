448ee0f3026456d430138f6e956b70a52c3fd7b1
lilwanngg <lillianwang@utexas.edu>
2019-08-20 01:37:27 -0400

    Test server.js file

--- bin/server.js
+++ bin/server.js
@@ -2,23 +2,23 @@ var app = require('../app');
 var debug = require('debug')
 var http = require('http');
 
-var port = normalizePort(process.env.PORT || '8080');
+var port = process.env.PORT || '8080';
 app.set('port', port);
 var server = http.createServer(app);
 
 server.listen(port);
 server.on('listening', onListening);
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
+// function normalizePort(val) {
+//   var port = parseInt(val, 10);
+//   if (isNaN(port)) {
+//     return val;
+//   }
+//   if (port >= 0) {
+//     return port;
+//   }
+//   return false;
+// }
 
 
 function onListening() {
