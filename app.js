06d61125b55b2cedbb54238d95667fbb656799d2
lilwanngg <lillianwang@utexas.edu>
2019-08-20 21:14:12 -0400

    Change callback address

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
@@ -10,15 +9,9 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname));
-// app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
