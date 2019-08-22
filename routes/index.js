8adb0c982b59f3ae9785a758bbaee46cad6600a4
lilwanngg <lillianwang@utexas.edu>
2019-08-22 16:13:21 -0400

    fix sizing issue

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var spotifyBackend = require('../backend/spotify-api-calls');
-var clusterMaker = require('clusters')
 
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -19,11 +18,10 @@ router.get('/callback', function(req, res) {
 
 
 router.get('/yourmud', function(req, res, next) {
-    const { audio_features, recommendations, clusters } = req.session
+    const { audio_features, recommendations } = req.session
     res.render('visualization', {
         audio_features: audio_features, 
-        recommendations: recommendations,
-        clusters: clusters
+        recommendations: recommendations
     })
 })
 
